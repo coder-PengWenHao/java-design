0efc166d2cbe4ba49d24a8a3d3fdac56106b3384
coderpwh <pwhfbc@outlook.com>
2023-09-15 13:47:35 +0800

    feat: add ThreadSafeLazyLoadedlvoryTower

--- src/main/java/com/coderpwh/design/singleton/ThreadSafeLazyLoadedIvoryTower.java
+++ src/main/java/com/coderpwh/design/singleton/ThreadSafeLazyLoadedIvoryTower.java
@@ -7,4 +7,27 @@ package com.coderpwh.design.singleton;
 public class ThreadSafeLazyLoadedIvoryTower {
 
 
+    private static volatile ThreadSafeLazyLoadedIvoryTower instance;
+
+
+    /***
+     * 私有构造函数
+     */
+    private ThreadSafeLazyLoadedIvoryTower() {
+
+    }
+
+
+    /***
+     * 饿汉式
+     * @return
+     */
+    public static synchronized ThreadSafeLazyLoadedIvoryTower getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeLazyLoadedIvoryTower();
+        }
+        return instance;
+    }
+
+
 }
